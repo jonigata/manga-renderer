8d56d9c1d7e72c8a815d42bba6223cfd36a7ed30
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2025-09-11 11:08:50 +0900

    bump

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Create symbolic link to FramePlanner source
 mkdir -p src/lib
-ln -s ../../../FramePlanner/src/lib/layeredCanvas src/lib/layeredCanvas
-ln -s ../../../FramePlanner/src/lib/book src/lib/book
+ln -s ../../../FramePlanner2/src/lib/layeredCanvas src/lib/layeredCanvas
+ln -s ../../../FramePlanner2/src/lib/book src/lib/book
 
 echo "Setup completed successfully!"
